275e67de809673a109f692604f276ef41f9d92e5
Christopher Foo <chris.foo@gmail.com>
2016-03-06 02:19:10 -0500

    Update example token analyze loop script

--- scripts/tokenanalyze_loop.sh
+++ scripts/tokenanalyze_loop.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 while true; do
-    python3 -m tppip.tokenanalyze > /tmp/tokenanalyze-new.json
+    python3 -m tppip.tokenanalyze \
+        --url-cache-filename /tmp/tppip_stream_url_cache.txt \
+        --save-screenshot-filename /tmp/tpp-current.png \
+        > /tmp/tokenanalyze-new.json
 
     if [ $? -eq 0 ]; then
         mv /tmp/tokenanalyze-new.json /tmp/tokenanalyze.json 
-        sleep 60
+        sleep 15
     else
         sleep 300
     fi
